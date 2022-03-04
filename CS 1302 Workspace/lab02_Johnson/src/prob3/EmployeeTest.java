4afac2c1c25107183e9d67644495ba30a070f75d
Megan Johnson <57232347+IamMegan@users.noreply.github.com>
2022-03-04 12:47:46 -0500

    Delete CS 1302 Workspace directory

--- CS 1302 Workspace/lab02_Johnson/src/prob3/EmployeeTest.java
+++
@@ -1,9 +0,0 @@
-package prob3;
-
-public class EmployeeTest {
-	public static void main(String[] args) {
-		String name = "Xavier";
-		HourlyEmployee e = new HourlyEmployee(name);
-		System.out.println(e);
-	}
-}
--- CS 1302 Workspace/lab02_Johnson/src/prob4/EmployeeTest.java
+++
@@ -1,9 +0,0 @@
-package prob4;
-
-public class EmployeeTest {
-	public static void main(String[] args) {
-		String name = "Xavier";
-		HourlyEmployee e = new HourlyEmployee(name);
-		System.out.println(e);
-	}
-}
