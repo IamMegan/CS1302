1780254e0e9f79eb2728ec785d4d28c5f32699a4
Megan Johnson <57232347+IamMegan@users.noreply.github.com>
2022-04-25 11:39:08 -0400

    Add files via upload

--- hw11_JohnsonM/src/student/AirportManager.java
+++ hw11_JohnsonM/src/student/AirportManager.java
@@ -17,13 +17,66 @@ public class AirportManager {
 	
 	public Airport getAirportClosestTo(String code) {
 		return null;
+		//TODO
+	}
+	
+	public List<Airport> getAirportsClosestTo(String code, int num){
+		return null;
+		//TODO
 	}
 	
 	public List<Airport> getAirports(){
-		List<Airport> airportsList = new ArrayList<Airport>();
-		for(Airport airport : this.airports.values()) {
-			airportsList.add(airport);
-		}
-		return airportsList;
+		return List<Airport> airportsList = new ArrayList<Airport>(airports.values());
 	}
+	
+	public List<Airport> getAirportsByCity(String city){
+		List<Airport> cityList = new ArrayList<Airport>();
+		for(Airport a : airports.values()) {
+			if(a.getCity.equals(city)) {
+				cityList.add(a);
+			}
+		}
+		return cityList;
+	}
+	
+	public List<Airport> getAirportsByCityState(String city, String State){
+		List<Airport> cityStateList = new ArrayList<Airport>();
+		for(Airport a : airports.values()) {
+			if(a.getCity.equals(city) && a.getState.equals(state)) {
+				cityStateList.add(a);
+			}
+		}
+		return cityStateList;
+	}
+	
+	public List<Airport> getAirportsWithin(double withinDist, double lat, double lon){
+		return null;
+		//TODO
+	}
+	
+	public List<Airport> getAirportsWithin(double withinDist, String code){
+		return null;
+		//TODO
+	}
+	
+	public List<Airport> getAirportsWithin(double withinDist, String code1, String code2){
+		return null;
+		//TODO
+	}
+	
+	public double getDistanceBetween(Airport airport1, Airport airport2) {
+		return 0;
+		//TODO
+	}
+	
+	public double getDistanceBetween(String code1, String code2) {
+		return 0;
+		//TODO
+	}
+	
+	public List<Airport> getNWSNamedAirports(){
+		return null;
+		//TODO
+	}
+	
 }
