ffe0380451ad66451258bf2f987864c50b766dda
Megan Johnson <57232347+IamMegan@users.noreply.github.com>
2022-04-26 15:46:04 -0400

    Add files via upload

---
+++ hw11_JohnsonM/src/student/AirportManagerTest.java
@@ -0,0 +1,99 @@
+package student;
+
+public class AirportManagerTest {
+	public static void main(String[] args) {
+		
+	}
+	
+	public static void testGetAirport() {
+		System.out.println("Test getAirport------");
+		//TODO
+		System.out.println("Expected: ");
+		System.out.println("Actual: ");
+	}
+	
+	public static void testGetAirportClosestTo() {
+		System.out.println("Test getAirportClosestTo------");
+		//TODO
+		System.out.println("Expected: ");
+		System.out.println("Actual: ");
+	}
+	
+	public static void testGetAirports() {
+		System.out.println("Test getAirports------");
+		//TODO
+		System.out.println("Expected: ");
+		System.out.println("Actual: ");
+	}
+	
+	public static void testGetAirportsByCity() {
+		System.out.println("Test getAirportsByCity------");
+		//TODO
+		System.out.println("Expected: ");
+		System.out.println("Actual: ");
+	}
+	
+	public static void testGetAirportsByCityState() {
+		System.out.println("Test getAirportsByCityState------");
+		//TODO
+		System.out.println("Expected: ");
+		System.out.println("Actual: ");
+	}
+	
+	public static void testGetAirportsClosestTo() {
+		System.out.println("Test getAirportsClosestTo------");
+		//TODO
+		System.out.println("Expected: ");
+		System.out.println("Actual: ");
+	}
+	
+	public static void testGetAirportsSortedBy() {
+		System.out.println("Test getAirportsSortedBy------");
+		//TODO
+		System.out.println("Expected: ");
+		System.out.println("Actual: ");
+	}
+	
+	public static void testGetAirportsWithin1() {
+		System.out.println("Test getAirportsWithin1------");
+		//TODO
+		System.out.println("Expected: ");
+		System.out.println("Actual: ");
+	}
+	
+	public static void testGetAirportsWithin2() {
+		System.out.println("Test getAirportsWithin2------");
+		//TODO
+		System.out.println("Expected: ");
+		System.out.println("Actual: ");
+	}
+	
+	public static void testGetAirportsWithin3() {
+		System.out.println("Test getAirportsWithin3------");
+		//TODO
+		System.out.println("Expected: ");
+		System.out.println("Actual: ");
+	}
+	
+	public static void testGetDistnceBetween1() {
+		System.out.println("Test getDistanceBetween1------");
+		//TODO
+		System.out.println("Expected: ");
+		System.out.println("Actual: ");
+	}
+	
+	public static void testGetDistnceBetween2() {
+		System.out.println("Test getDistanceBetween2------");
+		//TODO
+		System.out.println("Expected: ");
+		System.out.println("Actual: ");
+	}
+	
+	public static void testGetNWSNamedAirports() {
+		System.out.println("Test getNWSNamedAirports------");
+		//TODO
+		System.out.println("Expected: ");
+		System.out.println("Actual: ");
+	}
+}
+	
